95adfe0ff36a0f2855b4bfe4f26a80ffcd6dd669
Matt Fellows <matt.fellows@onegeek.com.au>
2020-11-09 22:04:56 +1100

    feat: add new (unused) field to provider to show that removing it is not dangerous

--- src/Repositories/Product.cs
+++ src/Repositories/Product.cs
@@ -3,15 +3,18 @@ namespace Products
 {
     public class Product
     {
-        public Product(string id, string type, string name) {
+        public Product(string id, string type, string name, string version) {
             this.id = id;
             this.name = name;
             this.type = type;
+            this.version = version;
         }
         public string type { get; set; }
 
         public string name { get; set; }
 
         public string id { get; set; }
+
+        public string version { get; set; }
     }
 }
