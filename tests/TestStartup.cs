871de499ccef12f04d1fef9b89500b697a232d17
Botond Botos <botond.botos@gmail.com>
2024-05-23 23:16:11 +0200

    refactor: remove unnecessary usings

--- tests/TestStartup.cs
+++ tests/TestStartup.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using tests.Middleware;
